129b48a5696b27251ede47fb6fad0a3e4de835a6
Roopika Srinivas <13376391+roopikasrinivas@users.noreply.github.com>
2021-10-02 18:35:33 +0530

    Changes 1

--- src/App.java
+++ src/App.java
@@ -4,6 +4,8 @@ public class App {
 	    System.out.println("This is Roopika's DevOps Assigment");
 	    System.out.println("Changes from Dev1");
 	    System.out.println("Changes from Dev2");
-      System.out.println("Changes from featureA branch");
+      
+
+      	System.out.println("Changes 1 from Develop branch");
 	}
 }
